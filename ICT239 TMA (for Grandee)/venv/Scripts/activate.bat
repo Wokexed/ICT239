ad3c89832a8d496eaa4249a62e8df0cb28d895a8
wokexe <163087670+Wokexed@users.noreply.github.com>
2025-10-06 00:52:23 +0800

    home page bruh

--- ICT239 TMA (for Grandee)/venv/Scripts/activate.bat
+++ ICT239 TMA (for Grandee)/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\woke\Desktop\Repository\ICT239\ICT239 TMA (for Grandee)\venv
+set VIRTUAL_ENV=C:\Users\Ray Lee\OneDrive\Desktop\Repository\ICT239\ICT239 TMA (for Grandee)\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
